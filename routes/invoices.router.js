a0dc316bea6db9d22c9ec2080b489c5d2418a578
Andres Alvarez <andres.alvarez094@hotmail.es>
2021-12-28 13:15:01 -0500

    Created CRUD to Invoices

--- routes/invoices.router.js
+++ routes/invoices.router.js
@@ -1,13 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
+// GET
 router.get('/', (req, res) => {
     res.send('invoices')
 })
 
 
 router.get('/:invoiceId', (req, res) => {
-    res.send('invoices')
+    const { invoiceId } = req.params;
+    res.send('invoice', invoiceId);
 });
 
+// POST
+router.post('/:invoiceId', (req, res) => {
+    const { invoiceId } = req.params;
+    const body = req.body;
+    res.json({
+        invoiceId,
+        message: 'Created Invoice',
+        body
+    });
+});
+
+// PUT
+router.put('/:invoiceId', (req, res) => {
+    const { invoiceId } = req.params;
+    const body = req.body;
+    res.json({
+        invoiceId,
+        message: 'Update Invoice',
+        body
+    });
+});
+
+// PATCH
+router.patch('/:invoiceId', (req, res) => {
+    const { invoiceId } = req.params;
+    const body = req.body;
+    res.json({
+        invoiceId,
+        message: 'Update Invoice',
+        body
+    });
+});
+
+// DELETE
+router.patch('/:invoiceId', (req, res) => {
+    const { invoiceId } = req.params;
+    res.json({
+        message: `Delete Invoice ${invoiceId}`,
+    });
+});
 module.exports = router;
