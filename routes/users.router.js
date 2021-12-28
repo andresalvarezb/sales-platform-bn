9ad0739e8b76a75867d287e3aefe001a88ccfa97
Andres Alvarez <andres.alvarez094@hotmail.es>
2021-12-28 13:09:21 -0500

    Created CRUD for Users

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,17 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
+// GET
 router.get('/', (req, res) => {
-    res.send('users')
+    res.send('users');
 })
 
-
 router.get('/:userId', (req, res) => {
-    res.send('users')
+    const { userId } = req.params;
+    res.send('user', userId);
 });
 
-router.get('/:userId/invoices', (req, res) => {
-    res.send('users')
+// POST
+router.post('/:userId', (req, res) => {
+    const { userId } = req.params;
+    const body = req.body;
+    res.json({
+        userId,
+        message: 'Created User',
+        body
+    });
+});
+
+// PUT
+router.put('/:userId', (req, res) => {
+    const { userId } = req.params;
+    const body = req.body;
+    res.json({
+        userId,
+        message: 'Update User',
+        body
+    });
+});
+
+// PATCH
+router.patch('/:userId', (req, res) => {
+    const { userId } = req.params;
+    const body = req.body;
+    res.json({
+        userId,
+        message: 'Update User',
+        body
+    });
+});
+
+// DELETE
+router.patch('/:userId', (req, res) => {
+    const { userId } = req.params;
+    res.json({
+        message: `Delete User ${userId}`,
+    });
 });
 
 module.exports = router;
