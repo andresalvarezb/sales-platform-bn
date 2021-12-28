2345c43f4d9824b6987b3162378cef937de90d2e
Andres Alvarez <andres.alvarez094@hotmail.es>
2021-12-28 13:17:03 -0500

    Created path global

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const usersRouter = require('./users.router');
 const productsRouter = require('./products.router');
 const invoicesRouter = require('./invoices.router');
+const express = require('express');
 
 function routerApi(app) {
-    app.get('/user', usersRouter);
-    app.get('/products', productsRouter);
-    app.get('/invoices', invoicesRouter);
+    const router = express.Router();
+    app.use('/api', router);
+
+    router.get('/user', usersRouter);
+    router.get('/products', productsRouter);
+    router.get('/invoices', invoicesRouter);
 };
 
 module.exports = routerApi;
